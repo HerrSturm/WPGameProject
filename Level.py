39abede2fb071f99b726af6875fc78f236613dd3
HerrSturm <david.sturm@posteo.de>
2020-06-05 11:33:28 +0200

    - added ObjectType as Enum Class - bugfix for level loading

--- Level.py
+++ Level.py
@@ -1,6 +1,7 @@
 from Object import *
 from Brick import *
 from MovingBrick import *
+from ObjectTypes import ObjectType
 
 class Level:
     def __init__(self):
@@ -20,13 +21,13 @@ class Level:
         self.objects = self.levelList[self.currentLevel]
 
     def update(self, screen):
-        for obj in self.currentLevel.objects:
+        for obj in self.objects:
             obj.update(self)
             obj.draw(screen)
 
 level1 = [
-            Brick((10,10),(4,2),"Brick"),
-            Brick((10,20),(4,2),"Brick"),
-            MovingBrick((10,14),(2,4),"Brick",(1,0)),
-            Brick((25,14),(4,2),"Brick")
+            Brick((10,10),(4,2),ObjectType.BRICK),
+            Brick((10,20),(4,2),ObjectType.BRICK),
+            MovingBrick((10,14),(2,4),ObjectType.BRICK,(1,0)),
+            Brick((25,14),(4,2),ObjectType.BRICK)
         ]
---
+++ ObjectTypes.py
@@ -0,0 +1,10 @@
+from enum import Enum
+"""Dient der Festlegung der verschiedenen Objekttypen,
+
+
+class ObjectType(Enum):
+    STAB = 0
+    BALL = 1
+    BRICK = 2
+    SOLID = 3
+    DEADLY = 4
