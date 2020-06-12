5c93c2142f0b0eb6c0927d65ef79cdc9c14c70a1
Si-Tr <simona.troeger@web.de>
2020-06-12 13:22:26 +0200

    letzter feinschliff

--- Stab.py
+++ Stab.py
@@ -3,8 +3,8 @@ from Object import *
 class Stab(GameObject):
 
     def update(self, gameManager):
-        self.move
-        self.draw
+        self.move()
+
 
     def draw(self,screen):
         pygame.draw.rect(screen, (255, 255, 255), self.box)
