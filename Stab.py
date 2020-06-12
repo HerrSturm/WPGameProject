44093b87b902add9b2f02c2a8174ec6091639cf2
Si-Tr <simona.troeger@web.de>
2020-06-12 13:04:29 +0200

    Schritte das gezeichnet werden kann
    änderung im Konstrucktor, der update funktion, der draw funktion.

--- Stab.py
+++ Stab.py
@@ -3,14 +3,15 @@ from Object import *
 class Stab(GameObject):
 
     def __init__(self, pos, size):
-        #super().__init__(.....) Nutze die Vererbung. Die Klasse erbt von GameObject
-        pass
+        super().__init__(self.box) 
+        
 
     def update(self, gameManager):
-        pass
+        self.move
+        self.draw
 
     def draw(self,screen):
-        pass
+        pygame.draw.rect(screen, (255, 255, 255), self.box)
 
     def move(self):
         pass
