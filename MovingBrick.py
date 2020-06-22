b124051a37afc74f60dbdcf425470f8381035f0d
F D <forscher-vt@gmx.de>
2020-06-22 12:59:05 +0200

    kollision
    -Kollisionsabfrage mit dem Bildschirmrand
    -Kollisionsabfrage mit anderen Objekten aus dem GameManager weitergeführt

--- MovingBrick.py
+++ MovingBrick.py
@@ -1,16 +1,29 @@
 from Brick import *
 
 class MovingBrick(Brick):
-    def __init__(self,pos,size,type, dir = [0,0]):
-        super().__init__(pos,size, type)
+    def __init__(self, pos, size, type, dir = [0,0]):
+        super().__init__(pos, size, type)
         self.dir = dir
 
-    def move(self):
+#bewegt den MovingBrick und fragt ab ob er mit dem Bildschirmrand kollidiert
+#wenn er dies tut, bewegt er sich in die andere Richtung (nur auf der Achse, auf der es kollidiert)
+    def move(self, gameManager):
         self.box[0] += self.dir[0]
         self.box[1] += self.dir[1]
+        if self.box[0] > gameManager.screenSize[0] or self.box[0] < 0:
+            self.dir[0] * (-1)
+        if self.box[1] > gameManager.screenSize[1] or self.box[1] < 0:
+            self.dir[1] * (-1)
 
-    def collision(self,obj, gameManager):
-        pass
+#wenn der MovingBrick mit etwas kollidiert fragt er ab, was es ist
+#ist es der Ball, so wird der MovingBrick auf die Liste markedForDestruction gesetzt
+#kollidiert er mit einem anderen Objekt, so bewegt er sich zurrück
+    def collision(self, obj, gameManager):
+        if obj.ObjectType == BALL:
+            gameManager.deleteObj(self)
+        else:
+            self.dir[0] * (-1)
+            self.dir[1] * (-1)
 
     def update(self, gameManager):
-        self.move()
+        self.move(gameManager)
